977c289e1c049919ec2ebbd38bd6e53c1e8c54d6
Jean-Christophe Grondin <groj2904@dinf-0014-13b.DInf.fsci.usherbrooke.ca>
2015-01-30 16:21:50 -0500

    Work in progress ...

--- cherrypy/bin/startCIDE.py
+++ cherrypy/bin/startCIDE.py
@@ -1,8 +1,27 @@
-import cherrypy
+import sys, os
+from cherrypy import (config as server_conf, 
+                      quickstart)
 from cide.index import Welcome
 
-# Read configuration ...
 
-# starts on 8080
-#cherrypy.config.update({'server.socket_host': '0.0.0.0'})
-cherrypy.quickstart(Welcome())
+from pdb import set_trace as dbg
+
+#conf = dict()
+#conf['server.socket_host'] = '0.0.0.0'
+#conf['server.socket_port'] = 8080
+#conf['server.threadPool'] = 20 # does not seems to work
+#conf['server.threadpool'] = 20
+
+conf = {
+  '/': {
+    'tools.sessions.on': True,
+     'tools.staticdir.root': os.path.abspath(os.getcwd())
+  },
+  '/static': {
+    'tools.staticdir.on': True,
+    'tools.staticdir.dir': './public'
+  }
+}
+
+server_conf.update(conf)
+quickstart(Welcome(), "/", conf)
--- cherrypy/src/index.py
+++ cherrypy/src/index.py
@@ -1,6 +1,18 @@
 import cherrypy
-class Welcome(object):
-    def index(self):
-        return "Hello World!"
-    index.exposed = True
+from genshi.template import TemplateLoader
 
+from time import sleep
+from pdb import set_trace as dbg
+
+template_dir = "templates"
+loader = TemplateLoader(template_dir, auto_reload=True, max_cache_size=100)
+ 
+class Welcome(object):
+    
+    @cherrypy.expose
+    def index(self):
+        sleep(5)
+        return "hello4"
+        #dbg()
+        #tmpl = loader.load('template1.html')
+        #return tmpl
