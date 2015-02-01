c8e722fc2e2569f08808e09791557f51b70f4671
Jean-Christophe Grondin <jean-christophe.grondin@usherbrooke.ca>
2015-02-01 09:06:25 -0500

    Adding html templates

--- cherrypy/src/index.py
+++ cherrypy/src/index.py
@@ -1,3 +1,4 @@
+import os
 import cherrypy
 from genshi.template import TemplateLoader
 
@@ -6,7 +7,8 @@ from pdb import set_trace as dbg
 
 template_dir = "templates"
 loader = TemplateLoader(template_dir, auto_reload=True, max_cache_size=100)
- 
+loader = TemplateLoader(os.path.join(os.path.dirname(__file__), 'templates'),
+                                 auto_reload=True) 
 class Welcome(object):
     
     @cherrypy.expose
@@ -17,6 +19,15 @@ class Welcome(object):
 
     @cherrypy.expose
     def gen(self):
-        dbg()
         tmpl = loader.load('template1.html')
-        return tmpl
+        # set args in generate as key1=val1, key2=val2
+        stream =  tmpl.generate()
+        return stream.render('html')
+
+    @cherrypy.expose
+    def gen2(self):
+        tmpl = loader.load('template2.html')
+        # set args in generate as key1=val1, key2=val2
+        stream =  tmpl.generate(var1='banana')
+        return stream.render('html')
+
